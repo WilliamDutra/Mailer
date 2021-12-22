2db4f4789c6015310d93b1b4ba5c2320ade5b272
William Dutra <will.dutra26@gmail.com>
2021-12-22 19:40:14 -0300

    ajuste para inciar o robo via cmd

--- Mailer.CLI/Program.cs
+++ Mailer.CLI/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Mailer.Domain.Interfaces;
 using Mailer.Domain.Services;
+using Mailer.Model.ValueObjects;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
 
 namespace Mailer.CLI
 {
@@ -9,12 +11,16 @@ namespace Mailer.CLI
     {
         static void Main(string[] args)
         {
-           
+
             try
             {
                 var serviceCollection = new ServiceCollection();
                 Startup.Register(serviceCollection);
-                                
+
+                var retorno = Run(args);
+
+                if (!retorno.Sucesso)
+                    Console.WriteLine($"{retorno.Mensagem} \n {retorno.Excecao.Message}");
 
             }
             catch (Exception)
@@ -22,6 +28,19 @@ namespace Mailer.CLI
 
                 throw;
             }
+
+        }
+
+        private static RetornoDoEnvioDoEmail Run(string[] args)
+        {
+            int totalArgs = args.Count();
+            var mail = Startup.GetService<IMail>();
+
+            string Para = args[0];
+            string Assunto = args[1];
+            string Corpo = args[2];
+
+            return mail.Enviar(Para, Assunto, Corpo);
 
         }
     }
